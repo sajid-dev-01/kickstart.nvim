7a6a6022d357ec4518b05da464158e567d579984
sajid-dev-01 <sajid.dev.01@gmail.com>
2024-12-11 15:14:59 +0600

    refactor(refactoring.nvim): refactor keys alignment and config property

--- lua/custom/plugins/refactoring.lua
+++ lua/custom/plugins/refactoring.lua
@@ -7,122 +7,39 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
     },
+    -- stylua: ignore
     keys = {
-      {
-        '<leader>rs',
-        mode = 'v',
-        desc = 'Refactor',
-      },
-      {
-        '<leader>rf',
-        function()
-          require('refactoring').refactor 'Extract Function'
-        end,
-        mode = 'x',
-        desc = 'Extract Function',
-      },
-      {
-        '<leader>rF',
-        function()
-          require('refactoring').refactor 'Extract Function To File'
-        end,
-        mode = 'x',
-        desc = 'Extract Function To File',
-      },
-      {
-        '<leader>rI',
-        function()
-          require('refactoring').refactor 'Inline Function'
-        end,
-        mode = 'x',
-        desc = 'Inline Function',
-      },
-      {
-        '<leader>rv',
-        function()
-          require('refactoring').refactor 'Extract Variable'
-        end,
-        mode = 'v',
-        desc = 'Extract Variable',
-      },
-      {
-        '<leader>ri',
-        function()
-          require('refactoring').refactor 'Inline Variable'
-        end,
-        mode = { 'n', 'x' },
-        desc = 'Inline Variable',
-      },
-      {
-        '<leader>rb',
-        function()
-          require('refactoring').refactor 'Extract Block'
-        end,
-        desc = 'Extract Block',
-      },
-      {
-        '<leader>rB',
-        function()
-          require('refactoring').refactor 'Extract Block To File'
-        end,
-        desc = 'Extract Block To File',
-      },
-      {
-        '<leader>rP',
-        function()
-          require('refactoring').debug.printf { below = false }
-        end,
-        desc = 'Debug Print',
-      },
-      {
-        '<leader>rp',
-        function()
-          require('refactoring').debug.print_var { normal = true }
-        end,
-        desc = 'Debug Print Variable',
-      },
-      {
-        '<leader>rc',
-        function()
-          require('refactoring').debug.cleanup {}
-        end,
-        desc = 'Debug Cleanup',
-      },
-      {
-        '<leader>rp',
-        function()
-          require('refactoring').debug.print_var()
-        end,
-        mode = 'v',
-        desc = 'Debug Print Variable',
-      },
+      { '<leader>rf', function() require('refactoring').refactor 'Extract Function' end,         desc = 'Extract Function', mode = 'x' },
+      { '<leader>rF', function() require('refactoring').refactor 'Extract Function To File' end, desc = 'Extract Function To File', mode = 'x' },
+      { '<leader>rI', function() require('refactoring').refactor 'Inline Function' end,          desc = 'Inline Function', mode = 'x' },
+      { '<leader>rv', function() require('refactoring').refactor 'Extract Variable' end,         desc = 'Extract Variable', mode = 'v' },
+      { '<leader>ri', function() require('refactoring').refactor 'Inline Variable' end,          desc = 'Inline Variable', mode = { 'n', 'x' } },
+      { '<leader>rb', function() require('refactoring').refactor 'Extract Block' end,            desc = 'Extract Block' },
+      { '<leader>rB', function() require('refactoring').refactor 'Extract Block To File' end,    desc = 'Extract Block To File' },
+      { '<leader>rP', function() require('refactoring').debug.printf { below = false } end,      desc = 'Debug Print' },
+      { '<leader>rp', function() require('refactoring').debug.print_var { normal = true } end,   desc = 'Debug Print Variable' },
+      { '<leader>rc', function() require('refactoring').debug.cleanup {} end,                    desc = 'Debug Cleanup' },
     },
-    opts = {
-      prompt_func_return_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      prompt_func_param_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      printf_statements = {},
-      print_var_statements = {},
-      show_success_message = true, -- shows a message with information about the refactor on success
-      -- i.e. [Refactor] Inlined 3 variable occurrences
-    },
-    config = function(_, opts)
-      require('refactoring').setup(opts)
+    config = function()
+      require('refactoring').setup {
+        prompt_func_return_type = {
+          go = true,
+          java = true,
+          cpp = true,
+          c = true,
+        },
+        prompt_func_param_type = {
+          go = true,
+          java = true,
+          cpp = true,
+          c = true,
+        },
+        printf_statements = {},
+        print_var_statements = {},
+        show_success_message = true, -- shows a message with information about the refactor on success
+        -- i.e. [Refactor] Inlined 3 variable occurrences
+      }
+
       require('telescope').load_extension 'refactoring'
     end,
   },
