8ec9b37964ad6895767a2ea169750cec704cfca1
sajid-dev-01 <sajid.dev.01@gmail.com>
2024-12-14 13:11:26 +0600

    feat(telescope): add keymap for search available  registers and marks

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -91,6 +91,8 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s"', builtin.registers, { desc = '[S]earch registers' })
+      vim.keymap.set('n', "<leader>s'", builtin.marks, { desc = '[S]earch marks' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
